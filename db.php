c819bccf5b155150ff9ebd081c8d6b9824184b21
rajinachaudhari <rajinachaudhari97@gmail.com>
2026-07-27 22:35:46 +0545

    :zap: fix: comment fixed

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-// db.php - Database connection (Only 4 lines to memorize!)
+// db.php - Database connection 
 $conn = mysqli_connect("localhost", "root", "", "student_db");
 if (!$conn) {
     die("Connection Failed: " . mysqli_connect_error());
